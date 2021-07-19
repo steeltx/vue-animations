5a0c1cba319553d1ffff60f77df969bf0566541b
steeltx <realsteel.gnu@gmail.com>
2021-07-18 23:12:10 -0500

    Animación con CSS

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <button type="button" @click="flag = !flag">Toogle</button>
-  <transition name="fade" mode="out-in">
+  <!-- <transition name="fade" mode="out-in">
     <h2 v-if="flag" key="main">Hola mundo!</h2>
     <h2 v-else key="secondary">Another hello!</h2>
+  </transition> -->
+  <transition name="zoom" type="animation" appear>
+    <h2 v-if="flag">Hello</h2>
   </transition>
 </template>
 
@@ -11,22 +14,64 @@ export default {
   name: "App",
   data() {
     return {
-      flag: false,
+      flag: true,
     };
   },
 };
 </script>
 
 <style>
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+
 .fade-enter-from {
   opacity: 0;
 }
 .fade-enter-active {
-  transition: all 3s linear;
+  transition: all 1s linear;
 }
 
 .fade-leave-to {
-  transition: all 3s linear;
+  transition: all 1s linear;
   opacity: 0;
+}
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 2s linear;
+}
+
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 2s linear;
+}
+
+.zoom-enter-from {
+  opacity: 0;
+}
+
+.zoom-leave-to {
+  opacity: 0;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
 }
 </style>
